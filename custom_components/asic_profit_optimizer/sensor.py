03175b8d9dcba4f2796495e15ac3b969e1a6defe
Paul <pwschattenberg@users.noreply.github.com>
2026-09-07 17:15:43 +0200

    v0.3.4: add distinct profitability sensor icons

--- custom_components/asic_profit_optimizer/sensor.py
+++ custom_components/asic_profit_optimizer/sensor.py
@@ -12,23 +12,40 @@ from .const import DOMAIN
 from .entity import AsicProfitEntity
 
 DEFINITIONS = {
-    "current_revenue": ("Current Revenue", None, None),
-    "current_electricity_cost": ("Current Electricity Cost", None, None),
-    "current_profit": ("Current Profit", None, None),
-    "optimal_power": ("Optimal Power", UnitOfPower.WATT, SensorDeviceClass.POWER),
+    "current_revenue": ("Current Revenue", None, None, "mdi:cash-plus"),
+    "current_electricity_cost": (
+        "Current Electricity Cost",
+        None,
+        None,
+        "mdi:transmission-tower",
+    ),
+    "current_profit": ("Current Profit", None, None, "mdi:cash"),
+    "optimal_power": (
+        "Optimal Power",
+        UnitOfPower.WATT,
+        SensorDeviceClass.POWER,
+        "mdi:flash",
+    ),
     "optimal_actual_power": (
         "Optimal Actual Power",
         UnitOfPower.WATT,
         SensorDeviceClass.POWER,
+        "mdi:power-plug",
     ),
-    "optimal_hashrate": ("Optimal Hashrate", "TH/s", None),
-    "optimal_efficiency": ("Optimal Efficiency", "W/TH", None),
-    "optimal_profit": ("Optimal Profit", None, None),
-    "optimal_daily_profit": ("Optimal Daily Profit", None, None),
+    "optimal_hashrate": ("Optimal Hashrate", "TH/s", None, "mdi:speedometer"),
+    "optimal_efficiency": ("Optimal Efficiency", "W/TH", None, "mdi:gauge"),
+    "optimal_profit": ("Optimal Profit", None, None, "mdi:chart-line"),
+    "optimal_daily_profit": (
+        "Optimal Daily Profit",
+        None,
+        None,
+        "mdi:calendar-today",
+    ),
     "break_even_electricity_price": (
         "Break-even Electricity Price",
         None,
         None,
+        "mdi:scale-balance",
     ),
 }
 
@@ -58,7 +75,7 @@ class AsicProfitSensor(AsicProfitEntity, SensorEntity):
 
     _attr_state_class = SensorStateClass.MEASUREMENT
 
-    def __init__(self, manager, key, name, unit, device_class) -> None:
+    def __init__(self, manager, key, name, unit, device_class, icon) -> None:
         super().__init__(manager, key)
         self.key = key
         self._attr_name = name
@@ -66,6 +83,7 @@ class AsicProfitSensor(AsicProfitEntity, SensorEntity):
             unit = f"{manager.hass.config.currency}/{suffix}"
         self._attr_native_unit_of_measurement = unit
         self._attr_device_class = device_class
+        self._attr_icon = icon
 
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(
